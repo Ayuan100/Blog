5a03e654457d11686518583669d7c1d1cb05eecc
Ayuan100 <369278452@qq.com>
2017-08-23 10:59:22 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,16 @@
+module.exports = {
+		host: "localhost",
+		mongodb: 'mongodb://localhost/test',
+		env: process.env.NODE_ENV || 'production',
+		schema: 'http://',
+    port: 3000,
+    mongodb: 'mongodb://localhost/tm-blog',
+    smtp: {
+        host: 'smtp.126.com',
+        port: 25,
+        auth: {
+            user: 'someone@example.com',
+            pass: 'password'
+        }
+    }
+};
